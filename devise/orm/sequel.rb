eb402c0ec9c4f1bbc8650ddc6768f6af21ac4c34
Mooman <janechii@gmail.com>
2010-04-10 23:10:50 -0700

    updated to work with Devise 1.1.rc0

--- devise/orm/sequel.rb
+++ devise/orm/sequel.rb
@@ -1,7 +1,66 @@
 module Devise
   module Orm
     module Sequel
-      module InstanceMethods
+      module Hook
+        def devise_modules_hook!
+          extend Schema
+          include Compatibility
+          yield
+        end
+      end
+
+      module Compatibility
+        extend ActiveSupport::Concern
+
+        module ClassMethods
+          # Hooks for confirmable
+          def before_create(*args)
+            wrap_hook(:before_create, *args)
+          end
+
+          def after_create(*args)
+            wrap_hook(:after_create, *args)
+          end
+
+          def wrap_hook(action, *args)
+            options = args.extract_options!
+
+            args.each do |callback|
+              class_eval <<-METHOD, __FILE__, __LINE__ + 1
+                def #{callback}
+                  super if #{options[:if] || true}
+                end
+
+                alias_method :orig_#{action}, :#{action}
+
+                def #{action}
+                  orig_#{action}
+                  #{callback}
+                end
+              METHOD
+            end
+          end
+
+          # Add ActiveRecord like finder
+          def find(*args)
+            options = args.extract_options!
+            conds = (options[:conditions].to_hash.symbolize_keys rescue true)
+
+            case args.first
+              when :first
+                first(conds)
+              when :all
+                filter(conds).all
+              else
+                primary_key_lookup(args.first)
+            end
+          end
+        end
+
+        def changed?
+          modified?
+        end
+
         def save(flag=nil)
           if flag == false
             raise unless save
@@ -9,69 +68,15 @@ module Devise
             super
           end
         end
-      end
 
-      def self.included_modules_hook(klass)
-        klass.send :extend,  self
-        klass.send :include, InstanceMethods
-
-        yield
-
-        klass.devise_modules.each do |mod|
-          klass.send(mod) if klass.respond_to?(mod)
+        def update_attributes(*args)
+          update(*args)
         end
-      end
-
-      include Devise::Schema
-
-      # Hooks for confirmable
-      def before_create(*args)
-        wrap_hook(:before_create, *args)
-      end
-
-      def after_create(*args)
-        wrap_hook(:after_create, *args)
-      end
-
-      def wrap_hook(action, *args)
-        options = args.extract_options!
-
-        args.each do |callback|
-          class_eval <<-METHOD, __FILE__, __LINE__ + 1
-            def #{callback}
-              super if #{options[:if] || true}
-            end
-
-            alias_method :orig_#{action}, :#{action}
-
-            def #{action}
-              orig_#{action}
-              #{callback}
-            end
-          METHOD
-        end
-      end
-
-      # Add ActiveRecord like finder
-      def find(*args)
-        options = args.extract_options!
-        case args.first
-          when :first
-            first(options[:conditions].to_hash.symbolize_keys || true)
-          when :all
-            filter(options[:conditions].to_hash.symbolize_keys || true).all
-          else
-            primary_key_lookup(args.first)
-        end
-      end
-
-      # Sequal Schema, does nothing at model level
-      def apply_schema(name, type, options={})
-        # return unless Devise.apply_schema
-        return
       end
 
       module Schema
+        include Devise::Schema
+
         SCHEMA_OPTIONS = { 
           :limit => :size
           # there's also precision and scale
@@ -101,8 +106,7 @@ end
 if defined?(Sequel) then
     # extend Sequel Model
     Sequel::Model.extend(Devise::Models)
-    # include devise schema macros
-    Sequel::Schema::Generator.send(:include, Devise::Schema)
+    Sequel::Model.extend(Devise::Orm::Sequel::Hook)
     # probably just need the apply_schema method
     Sequel::Schema::Generator.send(:include, Devise::Orm::Sequel::Schema)
 end
